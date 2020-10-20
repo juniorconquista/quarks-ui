8a51ea4e3e0656545f7cf57e0738dcaca4dfaaae
bernard Faria <bernardxf@gmail.com>
2020-10-20 08:35:14 -0300

    Removendo grays, white e black

--- src/ds/theme/colors.ts
+++ src/ds/theme/colors.ts
@@ -57,22 +57,5 @@ export default function (dark = false) {
                 light: '#343a40',
             };
         },
-        grays: {
-            100: '#f8f9fa',
-            200: '#e9ecef',
-            300: '#dee2e6',
-            400: '#ced4da',
-            500: '#adb5bd',
-            600: '#6c757d',
-            700: '#495057',
-            800: '#343a40',
-            900: '#212529',
-        },
-        black: {
-            default: '#000',
-        },
-        white: {
-            default: '#fff',
-        },
     };
 }
