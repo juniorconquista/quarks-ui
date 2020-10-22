a24b556a4eb908f47c7f70b46c1fce3b97018e81
bernard Faria <bernardxf@gmail.com>
2020-10-22 18:05:11 -0300

    adicionando favicon

--- src/gatsby-theme-docz/wrapper.js
+++ src/gatsby-theme-docz/wrapper.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 import { ThemeProvider } from 'styled-components';
-
+import favicon from './favicon.png';
 import theme from './styledTheme';
 
 export default ({ children }) => (
-    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    <ThemeProvider theme={theme}>
+        <Helmet>
+            <link rel="icon" type="image/png" href={favicon} />
+        </Helmet>
+        {children}
+    </ThemeProvider>
 );
