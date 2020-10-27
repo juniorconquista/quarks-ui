67e4742be9461fe1ed9bc9221a4fbf918813665d
Eduardo Costa <eduardo.costa@rarolabs.com.br>
2020-10-27 10:24:44 -0300

    Inserting colors for use in style

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import theme from './theme';
+import colors from './theme/colors';
 import * as hooks from './hooks/useTheme';
 import * as subatomicParticles from './subatomicParticles';
 import * as typography from './typography';
 
-export { theme, hooks, subatomicParticles, typography };
+export { colors, theme, hooks, subatomicParticles, typography };
