614659991e896c4ba609c0f9c3d23817997bd8ea
Júnior Conqusta <juniorconqusta@MacBook-Pro-de-Fabricio.local>
2020-10-27 17:07:26 -0300

    New version

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import theme from './theme';
 import colors from './theme/colors';
 import * as hooks from './hooks/useTheme';
-import * as subatomicParticles from './subatomicParticles';
-import * as typography from './typography';
 
-export { colors, theme, hooks, subatomicParticles, typography };
+export { colors, theme, hooks };
