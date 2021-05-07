a4bf02dc2bd5195ead31bf7b150e6c63b46e720f
Júnior Conqusta <juniorconqusta@MacBook-Pro-de-Fabricio.local>
2021-05-07 16:54:32 -0300

    chore: remove docz

--- doczrc.js
+++
@@ -1,8 +0,0 @@
-export default {
-    base: '/quarks-ui',
-    dest: '/public',
-    ignore: ['README.md', 'readme.md'],
-    files: ['./src/**/*.{markdown,mdx}'],
-    menu: ['Layout', 'Typography'],
-    typescript: true,
-};
